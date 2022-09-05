8ae966e61e4b14258a414ad8a8ed8cc8ae3d54a8
DuckoDas <100500099+DuckoDas@users.noreply.github.com>
2022-09-05 21:21:22 +0200

    Create mongodb-connecting.js

---
+++ Code Templates/mongodb-connecting.js
@@ -0,0 +1,20 @@
+Requirements:
+```
+npm i mongoose
+npm i discord.js@v14.3.0
+```
+
+```js
+const { MongoDB } = require("MongoDB URL");
+const { connect } = require("mongoose");
+
+module.exports = {
+  name: "ready",
+  once: true,
+  async execute(client) {
+    connect(MongoDB).then(() => {
+      console.log(`Mongoose Connected`);
+    });
+  },
+};
+```
